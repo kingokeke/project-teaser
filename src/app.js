f7d2c01822fc4574932dc26ad57c6e8f61ac1200
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-03 23:34:29 +0100

    Added express.json() parser

--- src/app.js
+++ src/app.js
@@ -3,9 +3,16 @@ const { userRoute, productRoute } = require('./routes');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 
-app.listen(3000, () => console.log('Server started'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Server started. API accessible at http://localhost:${PORT}/api/`)
+);
 
 module.exports = app;
