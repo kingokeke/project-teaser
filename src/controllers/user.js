fae4984c0899678dcd3c2c83b7759d38301c5b74
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-04 16:27:17 +0100

    Added signup functionality

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,7 +2,25 @@
 const { connection } = require('../database/connection');
 
 exports.signUp = (req, res) => {
-  connection.query('SHOW DATABASES', async function(err, results, fields) {
+  const { firstName, lastName, email, phone, username, password } = req.body;
+
+  const queryString =
+    "INSERT INTO `project-teaser`.`users` (`first_name`, `last_name`, `email`, `phone`, `username`, `password`) VALUES ('" +
+    firstName +
+    "', '" +
+    lastName +
+    "', '" +
+    email +
+    "', '" +
+    phone +
+    "', '" +
+    username +
+    "', '" +
+    password +
+    "');";
+
+  connection.query(queryString, function(err, results, fields) {
+    if (err) res.json({ errors: err });
     res.json({ message: results });
   });
 };
