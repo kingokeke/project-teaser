2e0374f674a658d0b8f184c75cbcf4d353f905be
Kingsley Okeke <mr.kingsley.okeke@gmail.com>
2019-08-04 10:43:50 +0100

    Set up and tested MySQL connection

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,15 +1,20 @@
-exports.getAllUsers = (req, res) => {
-  res.json({ message: 'This is the route to get all users' });
-};
+// const { queryDatabase } = require('../database/query');
+const { connection } = require('../database/connection');
 
 exports.signUp = (req, res) => {
-  res.json(req.body);
+  connection.query('SHOW DATABASES', async function(err, results, fields) {
+    res.json({ message: results });
+  });
+};
+
+exports.login = (req, res) => {
+  res.json({ message: `This is the route to login user ${req.params.id}` });
 };
 
 exports.update = (req, res) => {
-  res.json({ message: `This is the route for updating user ${req.params.id}` });
+  res.json({ message: `This is the route to update user ${req.params.id}` });
 };
 
 exports.delete = (req, res) => {
-  res.json({ message: `This is the route for deleting user ${req.params.id}` });
+  res.json({ message: `This is the route to delete user ${req.params.id}` });
 };
